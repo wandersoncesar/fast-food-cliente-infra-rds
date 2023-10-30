6d8efa7ec17fb7e0dbd4be67f2ff32104e291241
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2023-10-30 20:08:44 -0300

    Pipeline RDS

--- production.tf
+++ production.tf
@@ -16,7 +16,7 @@ module "rds" {
   database_name     = var.production_database_name
   database_username = var.production_database_username
   database_password = var.production_database_password
-  subnet_ids        = ["subnet-0732bdd5e5c7d90de", "subnet-0adfce1ac08c6b744"]
-  vpc_id            = "vpc-0dbe202c10e297c7f"
+  subnet_ids        = ["subnet-090645056d7342986", "subnet-07b6cfbf2e49b00f2"]
+  vpc_id            = "vpc-09e1933376ac822fc"
   instance_class    = "db.t3.micro"
 }
