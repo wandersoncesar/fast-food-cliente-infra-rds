7d85f5f23a888b5008dcff66e2906a0ede1ad227
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2023-10-30 08:32:00 -0300

    Atualizar pipeline

--- production.tf
+++ production.tf
@@ -16,7 +16,7 @@ module "rds" {
   database_name     = var.production_database_name
   database_username = var.production_database_username
   database_password = var.production_database_password
-  subnet_ids        = ["subnet-", "subnet-"]
-  vpc_id            = "vpc-"
+  subnet_ids        = ["subnet-0732bdd5e5c7d90de", "subnet-0adfce1ac08c6b744"]
+  vpc_id            = "vpc-0dbe202c10e297c7f"
   instance_class    = "db.t3.micro"
 }
