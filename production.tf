7303a731e4b218aae72050c945c410097d14da85
Wanderson César <contato@wandersoncesar.com.br>
2024-01-26 14:20:26 -0300

    Update production.tf

--- production.tf
+++ production.tf
@@ -16,7 +16,7 @@ module "rds" {
   database_name     = var.production_database_name
   database_username = var.production_database_username
   database_password = var.production_database_password
-  subnet_ids        = ["subnet-052bca6bb5af799b5", "subnet-0975413e62f9ae778"]
-  vpc_id            = "vpc-0b36947dc0287234c"
+  subnet_ids        = ["subnet-0ed872fb39094e628", "subnet-055ecb6a3ba4588f9"]
+  vpc_id            = "vpc-03599603e61e08c59"
   instance_class    = "db.t3.micro"
 }
